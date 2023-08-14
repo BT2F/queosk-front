fb654c140c2457535ea8c2ea9124d9fb3af9dc0a
SOPLAY <tkdgns1341@gmail.com>
2023-08-14 14:45:27 +0900

    Chore: init 누락 사항 업데이트

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,3 @@
-import { useState } from 'react'
-import { Book, Review } from '../mocks/types'
-
-type Props = {
-  book: Book
+export default function Home() {
+  return <div>랜딩 페이지</div>;
 }
-
-export default function Home({ book }: Props) {
-
-
-  return (
-    <div>랜딩 페이지</div>
-
-  )
-}
-
-
-
