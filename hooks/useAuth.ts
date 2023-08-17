f645028759ecc9717e0fe617ce01af8b20b85b4e
SOPLAY <tkdgns1341@gmail.com>
2023-08-17 17:51:31 +0900

    Feat : useAuth, useUser hooks 구현 및 axios 설정 (#2)
    - useAuth 구현
      - signUp, signIn, signUp 을 포함합니다
    - useUser 구현
      - 유저의 정보를 가져올 수 있고 수정할 수 있습니다.
    - axios interceptor 처리
      - axios interceptor를 통해 서버와 통신간에 header에 accessToken을 삽입합니다.
      - 서버와 통신해서 응답을 받았을때 status 코드가 401이 나올 경우 refreshToken을 사용해서 accessToken을 갱신후 재 요청 하는 로직을 추가합니다.
        - 만약 해당 accessToken 갱신이 실패할경우 저장된 모든 토큰을 삭제하는 처리를 추가합니다.
    - zustand devtools 수정
      - zustand의 미들웨어에서 devtools를 불러올 경우 prod 환경에서도 데브툴이 작동하기 때문에 커스텀 lib에 따로 작성했습니다.
        - @/lib/zustand/devtools.ts 파일에서 임포트해서 사용하면 됩니다.

---
+++ hooks/useAuth.ts
@@ -0,0 +1,57 @@
+import axios from '@/lib/axios';
+import { useRouter } from 'next/router';
+import { toast } from 'react-toastify';
+import { AUTH_MESSAGE } from '@/constants/auth';
+import useUser from '@/hooks/useUser';
+
+export default function useAuth() {
+  const router = useRouter();
+  const { setUser } = useUser();
+  const signIn = async (
+    data: { id?: string; password?: string; code?: string },
+    type: 'user' | 'restaurant' | 'OAuth' = 'user',
+  ) => {
+    if (type !== 'OAuth') {
+      await axios
+        .post(`/api/${type}/signin`, data)
+        .then((res) => {
+          const { accessToken, refreshToken, ...userData } = res.data;
+          setUser(userData);
+        })
+        .catch((e) =>
+          toast.error(e.response.data.message || AUTH_MESSAGE.SIGN_IN_ERROR),
+        );
+    } else {
+      // 현재는 경로가 존재하지 않음
+    }
+  };
+
+  const singUp = async (
+    data: {
+      id: string;
+      password: string;
+      email: string;
+      phone: string;
+      address?: string;
+      businessNum?: string;
+    },
+    type: 'restaurant' | 'user' = 'user',
+  ) =>
+    await axios.post(`/api/${type}/signup`, data).then(() => {
+      router.replace(type === 'user' ? '/' : '/mystore');
+    });
+
+  const signOut = async (type: 'restaurant' | 'user' = 'user') =>
+    await axios
+      .post(`/api/${type}/signout`)
+      .then(() => router.replace('/'))
+      .catch((e) =>
+        toast.error(e.response.data.message || AUTH_MESSAGE.SIGN_OUT_ERROR),
+      );
+
+  return {
+    signIn,
+    singUp,
+    signOut,
+  };
+}
--- lib/axios.ts
+++ lib/axios.ts
@@ -1,5 +1,76 @@
 import { default as axiosDefault } from 'axios';
+import { AUTH_KEY, AUTH_MESSAGE } from '@/constants/auth';
+import { toast } from 'react-toastify';
 
 const axios = axiosDefault.create();
+
+const isDev = process.env.NODE_ENV === 'development';
+// 요청 인터셉터
+axios.interceptors.request.use((config) => {
+  let token = localStorage.getItem(
+    config.url!.includes('token')
+      ? AUTH_KEY.REFRESH_TOKEN
+      : AUTH_KEY.ACCESS_TOKEN,
+  );
+
+  config.headers.Authorization = `Bearer ${token}`;
+
+  return config;
+});
+
+// 응답 인터셉터
+axios.interceptors.response.use(
+  (res) => {
+    if (res.config.url?.includes('signin')) {
+      localStorage.setItem(AUTH_KEY.ACCESS_TOKEN, res.data.accessToken);
+      localStorage.setItem(AUTH_KEY.REFRESH_TOKEN, res.data.refreshToken);
+    }
+    return res;
+  },
+  async (err) => {
+    const {
+      config,
+      response: { status },
+    } = err;
+
+    if (status === 401) {
+      // 비 인증 상태 일때
+      const target = config.url.includes('restaurant') ? 'restaurant' : 'user';
+      return await axios
+        .post(
+          `/api/${target}/access-token`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem(
+                AUTH_KEY.REFRESH_TOKEN,
+              )}`,
+            },
+          },
+        )
+        .then((res) => {
+          // 비인증 상태에서 성공적으로 토큰이 갱신된 경우
+          // 인증 정보를 저정한 이후 다시 요청을 보낸다.
+          localStorage.setItem(AUTH_KEY.ACCESS_TOKEN, res.data.accessToken);
+
+          config.headers.Authorization = `Bearer ${res.data.accessToken}`;
+
+          return axios(config);
+        })
+        .catch(() => {
+          // 비인증 상태에서 요청이 반려된 경우
+          // 인증 정보를 제거한다.
+          localStorage.removeItem(AUTH_KEY.ACCESS_TOKEN);
+          localStorage.removeItem(AUTH_KEY.REFRESH_TOKEN);
+
+          toast.error(AUTH_MESSAGE.REFRESH_ERROR);
+        });
+    }
+
+    isDev && console.error(`요청 에러 ( 에러코드 : ${status} ) `, err);
+
+    return Promise.reject(err);
+  },
+);
 
 export default axios;
