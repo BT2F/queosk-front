321ad0b4259d6688ba593e6d55bcdffe26045d45
SOPLAY <tkdgns1341@gmail.com>
2023-08-21 17:16:31 +0900

    Feat: Button, Input 공통 컴포넌트 추가
    - Input 컴포넌트의 경우 <Input/> 래핑을 통해 내부 로직을 통해 react.cloneElement를 통해 요소에 label과 같은 아이디를 주입합니다.

---
+++ components/Input.tsx
@@ -0,0 +1,65 @@
+import React, {
+  Children,
+  cloneElement,
+  ForwardedRef,
+  forwardRef,
+  HTMLAttributes,
+  InputHTMLAttributes,
+  ReactElement,
+} from 'react';
+import useId from '@/hooks/useId';
+
+interface InputProps extends HTMLAttributes<HTMLDivElement> {
+  label?: string;
+  children: ReactElement;
+  errorText?: string;
+}
+
+export default function Input({
+  label,
+  children,
+  errorText,
+  ...props
+}: InputProps) {
+  const child = Children.only(children);
+  const id = child.props.id || useId('input');
+  const isError = !!child.props.error ?? true;
+
+  return (
+    <div {...props} className="relative flex flex-col pb-6 gap-1 [&+&]:mt-2">
+      <label htmlFor={id}>{label}</label>
+      {cloneElement(child, { ...child.props, id })}
+      {isError && (
+        <span className="absolute bottom-0 text-red-400">{errorText}</span>
+      )}
+    </div>
+  );
+}
+
+interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  error?: boolean;
+  onEnter?: () => void;
+}
+
+Input.Field = forwardRef(
+  (
+    { error, onEnter = () => {}, ...props }: InputFieldProps,
+    ref: ForwardedRef<HTMLInputElement>,
+  ) => {
+    return (
+      <input
+        className={`w-full px-4 py-[6px] bg-gray-700/10 border border-gray-700/20 rounded duration-300 ${
+          error && 'border-red-500 bg-red-500/20'
+        }`}
+        ref={ref}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            e.preventDefault();
+            onEnter();
+          }
+        }}
+        {...props}
+      />
+    );
+  },
+);
