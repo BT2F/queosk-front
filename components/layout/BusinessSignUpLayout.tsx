9c753c84a7dfaa6b1404a62a3b532f5758dd5ce8
SOPLAY <tkdgns1341@gmail.com>
2023-08-22 23:02:06 +0900

    Feat: KAKAO 콜백, 사업자 로그인 및 회원가입 페이지 구현
    - KAKAO 콜백
      - 해당 기능의 경우 searchParams로 전달되는 code를 추출하여 이를 통해 인증을 인행할 예정입니다.

---
+++ components/layout/BusinessSignUpLayout.tsx
@@ -0,0 +1,32 @@
+import { HTMLAttributes } from 'react';
+
+const BusinessSignUpLayout = (props: HTMLAttributes<HTMLDivElement>) => (
+  <div className="my-10 p-10 max-w-[1050px] mx-auto" {...props} />
+);
+BusinessSignUpLayout.Form = (props: HTMLAttributes<HTMLFormElement>) => (
+  <form className="" {...props} />
+);
+BusinessSignUpLayout.Grid = (props: HTMLAttributes<HTMLDivElement>) => (
+  <div className="grid grid-cols-2 gap-x-10" {...props} />
+);
+BusinessSignUpLayout.Divider = () => (
+  <span className="divider !gap-2 text-gray-500">or</span>
+);
+
+BusinessSignUpLayout.Title = (props: HTMLAttributes<HTMLHeadElement>) => (
+  <h2 className="text-5xl font-bold" {...props} />
+);
+
+BusinessSignUpLayout.SubTitle = (
+  props: HTMLAttributes<HTMLParagraphElement>,
+) => <p className="mt-2 text-gray-600 mb-10" {...props} />;
+
+BusinessSignUpLayout.LinkContainer = (
+  props: HTMLAttributes<HTMLDivElement>,
+) => (
+  <div
+    className="flex mt-9 justify-center items-center duration-100 hover:scale-105"
+    {...props}
+  />
+);
+export default BusinessSignUpLayout;
