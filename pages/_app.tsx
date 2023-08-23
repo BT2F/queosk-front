cfdcae73c40c99beba3df12decef968581d0b3ae
SOPLAY <tkdgns1341@gmail.com>
2023-08-23 22:22:07 +0900

    Feat: RootLayout 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
-import { AppProps } from "next/app";
-import "../styles/globals.css";
-import { QueryClient } from "@tanstack/query-core";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { AppProps } from 'next/app';
+import '../styles/globals.css';
+import { QueryClient } from '@tanstack/query-core';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import RootLayout from '@/components/common/RootLayout';
 
-if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
-  require("../mocks");
+if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+  require('../mocks');
 }
 
 const client = new QueryClient({
@@ -20,7 +21,9 @@ const client = new QueryClient({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={client}>
-      <Component {...pageProps} />
+      <RootLayout>
+        <Component {...pageProps} />
+      </RootLayout>
       <ToastContainer />
     </QueryClientProvider>
   );
