0b0ffe2ef7381e43c2c37a646b965da0c881013e
Mason-HWChoe <hwchoe86@gmail.com>
2023-08-23 13:32:28 +0900

    커스텀 높이 지정 코드 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}"
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        112: '28rem',
+        120: '32rem',
+      },
+    },
   },
   plugins: [require('daisyui')],
-}
-
+};
