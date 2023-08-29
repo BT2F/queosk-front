772654a31f07612b3c2635005cb47557f0e648c4
SOPLAY <tkdgns1341@gmail.com>
2023-08-29 17:08:27 +0900

    Feat: 매장 정보 msw 작성 ( but API 문서에서 response가 대부분 작성되어 있지 않음 )

--- mocks/handlers/index.ts
+++ mocks/handlers/index.ts
@@ -1,5 +1,11 @@
 import { userHandler } from '@/mocks/handlers/userHandler';
 import { OAuthHandler } from '@/mocks/handlers/OAuthHandler';
 import { tokenHandler } from '@/mocks/handlers/tokenHandler';
+import { restaurantHandler } from '@/mocks/handlers/restaurantHandler';
 
-export const handlers = [...userHandler, ...OAuthHandler, ...tokenHandler];
+export const handlers = [
+  ...userHandler,
+  ...OAuthHandler,
+  ...tokenHandler,
+  ...restaurantHandler,
+];
