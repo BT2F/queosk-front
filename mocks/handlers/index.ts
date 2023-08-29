15930be2135e7bee922fe610a8d3cec006cc323c
SOPLAY <tkdgns1341@gmail.com>
2023-08-29 19:17:44 +0900

    Feat: 메뉴 정보 msw 작성

--- mocks/handlers/index.ts
+++ mocks/handlers/index.ts
@@ -2,10 +2,12 @@ import { userHandler } from '@/mocks/handlers/userHandler';
 import { OAuthHandler } from '@/mocks/handlers/OAuthHandler';
 import { tokenHandler } from '@/mocks/handlers/tokenHandler';
 import { restaurantHandler } from '@/mocks/handlers/restaurantHandler';
+import { menuHandler } from '@/mocks/handlers/menuHandler';
 
 export const handlers = [
   ...userHandler,
   ...OAuthHandler,
   ...tokenHandler,
   ...restaurantHandler,
+  ...menuHandler,
 ];
