e3f375fdd88a124872d75d5a41355d5d3a6a3e9e
ill2sam <mercone13@gmail.com>
2023-09-03 14:09:08 +0900

    Feat: 프로필 수정에서 사진을 클릭하면 사진 수정할 수 있는 input 기능 추가

--- components/account/Modal.tsx
+++ components/account/Modal.tsx
@@ -1,6 +1,12 @@
-import { ButtonHTMLAttributes, HTMLAttributes } from 'react';
+import React, {
+  ButtonHTMLAttributes,
+  HTMLAttributes,
+  InputHTMLAttributes,
+  ReactNode,
+  useRef,
+} from 'react';
 
-export default function AccountModal({
+export default function Modal({
   className,
   ...props
 }: HTMLAttributes<HTMLDivElement>) {
@@ -55,62 +61,94 @@ export default function AccountModal({
   );
 }
 
-AccountModal.BackGround = (props: HTMLAttributes<HTMLDivElement>) => (
+Modal.BackGround = (props: HTMLAttributes<HTMLDivElement>) => (
   <div
     className="modal-bg z-[99] fixed justify-center items-center bg-gray-400/50 top-0 left-0 right-0 bottom-0 flex"
     {...props}
   />
 );
 
-AccountModal.Body = (props: HTMLAttributes<HTMLDivElement>) => (
+Modal.Body = (props: HTMLAttributes<HTMLDivElement>) => (
   <div className="card w-[90%] max-w-lg shadow-md bg-white">
     <div className="card-body p-6">{props.children}</div>
   </div>
 );
 
-AccountModal.Header = (props: HTMLAttributes<HTMLHeadElement>) => (
+Modal.Header = (props: HTMLAttributes<HTMLHeadElement>) => (
   <h1 className="text-xl font-bold mb-4" {...props}></h1>
 );
 
-AccountModal.Form = (props: HTMLAttributes<HTMLFormElement>) => (
+Modal.Form = (props: HTMLAttributes<HTMLFormElement>) => (
   <form {...props}></form>
 );
 
-AccountModal.ProfileInfoContainer = (props: HTMLAttributes<HTMLDivElement>) => (
+Modal.ProfileInfoContainer = (props: HTMLAttributes<HTMLDivElement>) => (
   <div className="profile-info mb-10" {...props} />
 );
 
 interface ProfileImage extends HTMLAttributes<HTMLImageElement> {
   alt?: string;
   src?: string;
+  children?: ReactNode;
 }
 
-AccountModal.ProfilePicture = ({ alt, src, ...props }: ProfileImage) => (
-  <div className="profile-picture flex justify-center relative">
-    <div className="w-24 h-24 border rounded-full flex justify-center items-center">
-      <img alt={alt} src={src} {...props} />
+interface ProfileInput extends InputHTMLAttributes<HTMLInputElement> {}
+
+Modal.ProfileImg = ({ alt, src, children, ...props }: ProfileImage) => (
+  <div className="profile-picture flex justify-center">
+    <div className="w-24 h-24 border rounded-full flex justify-center items-center overflow-hidden">
+      <label htmlFor="image-upload-input">
+        <img
+          className="w-24 h-24 cursor-pointer object-cover"
+          alt={alt}
+          src={src}
+          {...props}
+        />
+      </label>
     </div>
+    {children}
   </div>
 );
 
-interface ProfileTextProps extends HTMLAttributes<HTMLInputElement> {
+Modal.ProfileInput = 
+  (props: ProfileInput) => (
+    <input
+      type="file"
+      accept="image/*"
+      id="image-upload-input"
+      className="hidden"
+      {...props}
+    />
+  )
+;
+
+interface ProfileTextProps extends InputHTMLAttributes<HTMLInputElement> {
   textHeader?: string;
+  validation?: boolean | null;
 }
 
-AccountModal.ProfileText = ({ textHeader, ...props }: ProfileTextProps) => (
+Modal.ProfileText = ({ textHeader, validation, ...props }: ProfileTextProps) => (
   <div className="profile-text text-left my-4">
     <div className="">
       {textHeader ? (
         <div className="mb-1 font-bold text-xs">{textHeader}</div>
       ) : null}
       <input className="w-full p-3 bg-gray-100 rounded-lg" {...props} />
+      {validation ? (
+        <p className='text-green-400 text-xs mt-2'>사용 가능한 닉네임입니다.</p>
+      ) : (
+        <p className='text-red-400 text-xs mt-2'>영문, 한글, 숫자를 시용하여 3~16자까지 입력하세요.</p>
+      )}
     </div>
   </div>
 );
 
-AccountModal.Btn = (props: ButtonHTMLAttributes<HTMLButtonElement>) => (
+Modal.Btn = (
+  props: ButtonHTMLAttributes<HTMLButtonElement>) => (
   <button
-    className={`btn bg-orange-200 hover:bg-orange-300 text-xs w-20 disabled:bg-gray-100 disabled:border-gray-200 ${props.className}`} onClick={props.onClick}
+    className={`btn bg-orange-200 hover:bg-orange-300 text-xs w-20 disabled:bg-gray-100 disabled:border-gray-200 ${props.className}`}
+    onClick={props.onClick}
+    disabled={props.disabled}
   >
     {props.children}
   </button>
--- components/views/account/Account.tsx
+++ components/views/account/Account.tsx
@@ -3,64 +3,115 @@ import AccountLayOut from '../../account/AccountLayout';
 import AccountProfileInfo from '../../account/AccountProfileInfo';
 import ProfileListHeader from '../../account/ProfileListHeader';
 import ProfileListItem from '../../account/ProfileListItem';
-import AccountModal from '../../account/AccountModal';
-import { useState } from 'react';
+import Modal from '../../account/Modal';
+import React, { useEffect, useState, ChangeEvent } from 'react';
+import axios from '@/lib/axios';
 
 export default function Account() {
   const [profileModal, setProfileModal] = useState(false);
   const [passwordModal, setPasswordModal] = useState(false);
+  const [ imgUrl, setImgUrl ] = useState('')
+  const [ nicknameValue, setNicknameValue ] = useState<string>('');
+  const [ validText, setValidText ] = useState<boolean | null>(null);
+  const [ userData, setUserData ] = useState(Object)
 
-  console.log(profileModal);
-  console.log(passwordModal);
+  // const axios = require('axios');
+  const serverUrl = 'http://localhost:3000'
+  
+  const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
+    const uploadedImage = e.target.files?.[0];
+    if(uploadedImage) {
+      const imageUrl = URL.createObjectURL(uploadedImage);
+      setImgUrl(imageUrl);
+    }
+  }
+
+  const handleNicknameChange =(e:ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setNicknameValue(value);
+
+    if(/^[a-z가-힣0-9]{3,16}/i.test(value)) {
+      setValidText(true)
+    } else {
+      setValidText(false)
+    }
+  }
+
+  console.log(validText)
+  
+  useEffect (() => {
+    const axiosUserData =  async () => {
+      try {
+        const response = await axios.get(`${serverUrl}/api/users`);
+        const data = response.data;
+        setUserData(data);
+        setImgUrl(userData.imageUrl);
+      } catch (error) {
+        console.error('데이터 로드 오류', error);
+      }
+    };
+    
+    axiosUserData();
+    } ,[])
+  
   return (
     <>
       {profileModal && (
-        <AccountModal>
-          <AccountModal.BackGround>
-            <AccountModal.Body>
+        <Modal>
+          <Modal.BackGround>
+            <Modal.Body>
               <AccountHeader children={'프로필 수정'} />
-              <AccountModal.Form>
-                <AccountModal.ProfileInfoContainer>
-                  <AccountModal.ProfilePicture alt={'프로필 이미지'} />
-                  <AccountModal.ProfileText
+              <Modal.Form>
+                <Modal.ProfileInfoContainer>
+                  <Modal.ProfileImg src={imgUrl} alt={'프로필 이미지'}>
+                    <Modal.ProfileInput onChange={handleImageUpload} />
+                  </Modal.ProfileImg>
+                  <Modal.ProfileText
                     textHeader={'닉네임'}
                     placeholder={'닉네임을 입력해주세요.'}
+                    pattern={'/^[a-z가-힣0-9]{3,16}/i'}
+                    value={nicknameValue}
+                    onChange={handleNicknameChange}
+                    validation={validText}
                   />
-                </AccountModal.ProfileInfoContainer>
+                </Modal.ProfileInfoContainer>
                 <div className="button-container flex justify-around">
-                  <AccountModal.Btn className={'w-2/5'} children={'수정'} />
-                  <AccountModal.Btn
+                  <Modal.Btn className={'w-2/5'} children={'수정'} disabled={
+                    validText === null || validText === false
+                  }/>
+                  <Modal.Btn
                     className={'w-2/5'}
                     children={'취소'}
                     type="button"
                     onClick={(e) => {
                       e.preventDefault();
+                      setImgUrl('');
                       setProfileModal(false);
                     }}
                   />
                 </div>
-              </AccountModal.Form>
-            </AccountModal.Body>
-          </AccountModal.BackGround>
-        </AccountModal>
+              </Modal.Form>
+            </Modal.Body>
+          </Modal.BackGround>
+        </Modal>
       )}
 
       {passwordModal && (
-        <AccountModal>
-          <AccountModal.BackGround>
-            <AccountModal.Body>
+        <Modal>
+          <Modal.BackGround>
+            <Modal.Body>
               <AccountHeader children={'비밀번호 변경'} />
-              <AccountModal.Form>
-                <AccountModal.ProfileInfoContainer>
-                  <AccountModal.ProfileText placeholder="새로운 비밀번호를 입력해주세요." />
-                  <AccountModal.ProfileText placeholder="새로운 비밀번호를 다시 입력해주세요." />
+              <Modal.Form>
+                <Modal.ProfileInfoContainer>
+                  <Modal.ProfileText placeholder="새로운 비밀번호를 입력해주세요." />
+                  <Modal.ProfileText placeholder="새로운 비밀번호를 다시 입력해주세요." />
                   <p className="text-xs px-2 text-gray-700">
                     {'비밀번호 조건'}을 조합하여 8~20자로 설정해주세요.
                   </p>
-                </AccountModal.ProfileInfoContainer>
+                </Modal.ProfileInfoContainer>
                 <div className="button-container flex justify-around">
-                  <AccountModal.Btn children={'변경'} className={'w-2/5'} />
-                  <AccountModal.Btn
+                  <Modal.Btn children={'변경'} className={'w-2/5'} />
+                  <Modal.Btn
                     children={'취소'}
                     className={'w-2/5'}
                     type="button"
@@ -70,17 +121,18 @@ export default function Account() {
                     }}
                   />
                 </div>
-              </AccountModal.Form>
-            </AccountModal.Body>
-          </AccountModal.BackGround>
-        </AccountModal>
+              </Modal.Form>
+            </Modal.Body>
+          </Modal.BackGround>
+        </Modal>
       )}
 
       <AccountLayOut>
         <AccountHeader children={'마이페이지'} />
         <AccountProfileInfo
-          profileNickName={'닉네임'}
-          profileNumber={'0105555****'}
+          imgUrl={userData.imageUrl}
+          profileNickName={userData.nickName}
+          profileNumber={userData.phone}
           className="mb-1"
         />
         <hr className="my-5 h-0.5" />
