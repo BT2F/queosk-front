bd78ff28c45edd122b2b7d2d7f2373a562816b13
SOPLAY <tkdgns1341@gmail.com>
2023-09-05 20:03:14 +0900

    Refactor: useAuth 리펙토링 진행
    - 로그인 혹은 회원가입 성공시 router replace로직을 useAuth hook으로 이동
    - 사업자 회원가입 기능 추가
    - OAuth 로그인 관련 기능 추가
    - OAuth 관련 리다이렉션 함수 추가

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -12,46 +12,78 @@ export default function useAuth() {
     type: 'user' | 'restaurant' | 'OAuth' = 'user',
   ) => {
     if (type !== 'OAuth') {
-      await axios
+      return await axios
         .post(`/api/${type}s/signin`, data)
         .then((res) => {
           const { accessToken, refreshToken, ...userData } = res.data;
           setUser(userData);
+          router.replace(type === 'user' ? '/store' : '/mystore');
         })
         .catch((e) =>
           toast.error(e.response.data.message || AUTH_MESSAGE.SIGN_IN_ERROR),
         );
     } else {
-      // 현재는 경로가 존재하지 않음
+      return await axios
+        .post('/api/kakao/signin', { code: data.code })
+        .then(() => {
+          router.replace('/store');
+        })
+        .catch((e) =>
+          toast.error(e.response.data.message || AUTH_MESSAGE.SIGN_IN_ERROR),
+        );
     }
   };
 
-  const singUp = async (
+  const signUp = async (
     data: {
-      id: string;
-      password: string;
-      email: string;
-      phone: string;
+      password?: string;
+      email?: string;
+      nickName?: string;
       address?: string;
       businessNum?: string;
+      ownerId?: string;
+      phone?: string;
+      ownerName?: string;
+      passwordCheck?: string;
+      restaurantPhone?: string;
+      restaurantName?: string;
+      category?: string;
+      businessNumber?: string;
+      businessStartDate?: string;
+      addressDetail?: string;
     },
     type: 'restaurant' | 'user' = 'user',
   ) =>
-    await axios.post(`/api/${type}/signup`, data).then(() => {
-      router.replace(type === 'user' ? '/' : '/mystore');
-    });
+    await axios
+      .post(`/api/${type}s/signup`, data)
+      .then(() =>
+        router.replace(type === 'user' ? '/signin' : '/mystore/signin'),
+      )
+      .catch((e) => {
+        toast.error(e.response.data.message || AUTH_MESSAGE.SIGN_IN_ERROR);
+      });
 
   const signOut = async (type: 'restaurant' | 'user' = 'user') =>
     await axios
-      .post(`/api/${type}/signout`)
+      .post(`/api/${type}s/signout`)
       .then(() => router.replace('/'))
       .catch((e) =>
         toast.error(e.response.data.message || AUTH_MESSAGE.SIGN_OUT_ERROR),
       );
 
+  const OAuthRedirect = (type: 'kakao' = 'kakao') => {
+    if (type === 'kakao') {
+      router.replace(
+        `https://kauth.kakao.com/oauth/authorize?client_id=${
+          process.env.NEXT_PUBLIC_KAKAO_CLIENT || ''
+        }&redirect_uri=http://localhost:3000/auth/kakao/callback&response_type=code`,
+      );
+    }
+  };
   return {
     signIn,
-    singUp,
+    signUp,
     signOut,
+    OAuthRedirect,
   };
 }
