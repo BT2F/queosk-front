085efee761f7b6c20475e86d3fec53678ea1d44f
Su-Bini <posb200819@gmail.com>
2023-09-10 21:26:06 +0900

    Test: api test

--- components/views/order_move/OrderListMoveView.tsx
+++ components/views/order_move/OrderListMoveView.tsx
@@ -1,170 +1,115 @@
 import AdditionalWaiting from '@/components/order_move/AdditionalWaiting';
 import CookedList from '@/components/order_move/CookedList';
-import LayoutState from '@/components/order_move/LayoutState';
 import OrderList from '@/components/order_move/OrderList';
 import { useState } from 'react';
+import axios from '@/lib/axios';
+import { useQuery } from '@tanstack/react-query';
 
-interface MenuType {
-  menuName: string;
-  quantity: number;
-  tableNumber?: number;
-  cookTime?: string;
+interface TableInfo {
+  tableId: number;
+  status?: 'USING' | 'OPEN';
+}
+
+interface MenuInfo {
+  id?: number;
+  restaurantId?: number;
+  name: string;
+  imageUrl?: string;
+  price?: number;
+  status?: 'SOLD_OUT' | 'ON_SALE';
 }
 
 interface OrderDataType {
-  tableNumber: number;
-  orderDateTime: string;
-  menu: MenuType[];
+  orderId: number;
+  table: TableInfo;
+  menu: MenuInfo;
+  orderStatus: 'IN_PROGRESS' | 'DONE' | 'CANCELED';
+  count: number;
 }
-
-interface CookedMenuList {
-  menu: MenuType[];
-  tableNumber: number;
-  cookTime: string;
-}
-
-const Data: OrderDataType[] = [
-  {
-    tableNumber: 2,
-    orderDateTime: '2023-08-15 12:30:00',
-    menu: [
-      {
-        menuName: '햄버거',
-        quantity: 2,
-      },
-      {
-        menuName: '감자튀김',
-        quantity: 3,
-      },
-    ],
-  },
-  {
-    tableNumber: 3,
-    orderDateTime: '2023-08-15 13:15:00',
-    menu: [
-      {
-        menuName: '피자',
-        quantity: 1,
-      },
-      {
-        menuName: '샐러드',
-        quantity: 2,
-      },
-    ],
-  },
-  {
-    tableNumber: 5,
-    orderDateTime: '2023-08-15 14:00:00',
-    menu: [
-      {
-        menuName: '스테이크',
-        quantity: 3,
-      },
-      {
-        menuName: '마카로니',
-        quantity: 1,
-      },
-    ],
-  },
-  {
-    tableNumber: 1,
-    orderDateTime: '2023-08-15 15:45:00',
-    menu: [
-      {
-        menuName: '샐러드',
-        quantity: 1,
-      },
-      {
-        menuName: '음료수',
-        quantity: 2,
-      },
-    ],
-  },
-  {
-    tableNumber: 6,
-    orderDateTime: '2023-08-15 16:30:00',
-    menu: [
-      {
-        menuName: '스파게티',
-        quantity: 2,
-      },
-      {
-        menuName: '디저트',
-        quantity: 1,
-      },
-    ],
-  },
-  {
-    tableNumber: 4,
-    orderDateTime: '2023-08-15 15:30:00',
-    menu: [
-      {
-        menuName: '피자',
-        quantity: 1,
-      },
-      {
-        menuName: '디저트',
-        quantity: 2,
-      },
-    ],
-  },
-  {
-    tableNumber: 7,
-    orderDateTime: '2023-08-15 17:30:00',
-    menu: [
-      {
-        menuName: '샐러드',
-        quantity: 2,
-      },
-      {
-        menuName: '햄버거',
-        quantity: 2,
-      },
-    ],
-  },
-];
 
 export default function OrderListMoveView() {
-  const [cookedMenuList, setCookedMenuList] = useState<CookedMenuList[]>([]);
+  const [cookedMenuList, setCookedMenuList] = useState<OrderDataType[]>([]);
+  const [orderData, setOrderData] = useState<OrderDataType[]>([]);
+
   const [visibleStateIndex, setVisibleStateIndex] = useState<number[]>(
-    Array.from({ length: Data.length }, (_, i) => i)
+    orderData.map((data) => data.orderId)
   );
+  //웨이팅 팀 수 및 팀 정보 확인
+  const { data: waitingStatus } = useQuery({
+    queryKey: ['waitingTeam'],
+    queryFn: async () => await axios.get('/api/restaurants/queue'),
+  });
+  console.log(waitingStatus?.data);
 
-  const handleCookCompleteClick = (
-    combinedData: {
-      menu: MenuType[];
-      tableNumber: number;
-      currentTime: string;
-    },
-    deleteIndex: number
-  ) => {
-    console.log(combinedData);
+  //주문 처리중 리스트 확인
+  const { data: todayOrderData } = useQuery({
+    queryKey: ['todayOrder'],
+    queryFn: async () => await axios.get('/api/restaurant/orders/today'),
+  });
+  console.log(todayOrderData?.data);
 
-    const { menu, tableNumber, currentTime } = combinedData;
+  // useEffect(() => {
+  //   refetchTodayOrder().then((data) => {
+  //     setOrderData(data?.data);
+  //     setVisibleStateIndex(
+  //       data?.data.map((data: OrderDataType) => data.orderId) || []
+  //     );
+  //   });
+  // }, [orderData, todayOrderData]);
 
-    const updatedMenuList = {
-      tableNumber,
-      cookTime: currentTime,
-      menu,
-    };
+  // //주문 처리 완료 리스트 확인
+  // const { data: cookedOrderData, refetch: refetchCookedOrder } = useQuery({
+  //   queryKey: ['cookedOrder'],
+  //   queryFn: async () => {
+  //     const response = await axios.get('/api/restaurant/orders/today');
+  //     console.log(response.data);
+  //     return response.data;
+  //   },
+  // });
 
-    setCookedMenuList((prevCookedMenuList) => [
-      ...prevCookedMenuList,
-      updatedMenuList,
-    ]);
+  // useEffect(() => {
+  //   refetchCookedOrder().then((data) => {
+  //     setCookedMenuList(data?.data);
+  //   });
+  // }, [cookedMenuList, cookedOrderData]);
 
-    setVisibleStateIndex((prevVisibleStateIndex) =>
-      prevVisibleStateIndex.filter((i) => i !== deleteIndex)
-    );
-  };
+  //주문 상태 수정
+
+  // const handleCookCompleteClick = (
+  //   combinedData: {
+  //     menu: MenuType[];
+  //     tableId: number;
+  //     currentTime: string;
+  //   },
+  //   deleteIndex: number
+  // ) => {
+  //   console.log(combinedData);
+
+  //   const { menu, tableId, currentTime } = combinedData;
+
+  //   const updatedMenuList = {
+  //     tableId,
+  //     cookTime: currentTime,
+  //     menu,
+  //   };
+
+  //   setCookedMenuList((prevCookedMenuList) => [
+  //     ...prevCookedMenuList,
+  //     updatedMenuList,
+  //   ]);
+
+  //   setVisibleStateIndex((prevVisibleStateIndex) =>
+  //     prevVisibleStateIndex.filter((i) => i !== deleteIndex)
+  //   );
+  // };
   return (
-    <div className="w-full h-[800px] flex flex-row justify-center px-[10px] py-[10px]">
-      <div className="w-2/3 h-auto overflow-y-scroll" id="scrollCustom">
-        <OrderList
+    <div className="w-full h-screen flex flex-row justify-center px-[10px] py-[10px] overflow-hidden">
+      <div className="w-2/3 overflow-y-scroll" id="scrollCustom">
+        {/* <OrderList
           visibleStateIndex={visibleStateIndex}
-          data={Data}
-          cookCompleteClick={handleCookCompleteClick}
-        />
+          data={orderData}
+          //cookCompleteClick={handleCookCompleteClick}
+        /> */}
         <style>
           {`
             #scrollCustom::-webkit-scrollbar {
@@ -181,9 +126,9 @@ export default function OrderListMoveView() {
             }`}
         </style>
       </div>
-      <div className="w-[250px]">
-        <AdditionalWaiting />
-        <CookedList cookedMenuList={cookedMenuList} />
+      <div className="w-[250px] h-screen">
+        {/* <AdditionalWaiting />
+        <CookedList cookedMenuList={cookedMenuList} /> */}
       </div>
     </div>
   );
