d6479fca714fd16189dbdecae7f06f7681983632
ill2sam <39899129+ill2sam@users.noreply.github.com>
2023-09-14 20:41:50 +0900

    fix: mywaiting 뒤 세미콜론 삭제, 및 실시간 확인 버튼 색상 변경. (#38)
    * Feat: root/myaiting에 나의 현재 웨이팅을 전체 다 볼 수 있는 마이웨이팅 페이지 추가
    
    * fix: mywaiting 뒤 세미콜론 삭제, 및 실시간 확인 버튼 색상 변경.

--- pages/mywaiting/index.tsx
+++ pages/mywaiting/index.tsx
@@ -1,14 +1,13 @@
-import dynamic from "next/dynamic";
-import { Suspense } from "react";
-const MyWaiting = dynamic(() => import('@/components/my_waiting/MyWaiting'),{
-  ssr: false
+import dynamic from 'next/dynamic';
+import { Suspense } from 'react';
+const MyWaiting = dynamic(() => import('@/components/my_waiting/MyWaiting'), {
+  ssr: false,
 });
 
 export default function Page() {
   return (
     <Suspense>
-
-    <MyWaiting />;
+      <MyWaiting />
     </Suspense>
-  )
+  );
 }
