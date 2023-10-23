2b75b12f0329cb47216e7e1ad37d5929d9e95250
SOPLAY <tkdgns1341@gmail.com>
2023-10-23 23:08:19 +0900

    Fix: Storybook의 뷰포트 기본 설정 변경

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,9 +3,14 @@ import { withThemeFromJSXProvider } from '@storybook/addon-themes';
 import BaseRadixTheme from './BaseRadixTheme';
 import '@/styles/globals.css';
 import './radux-ui.css';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 const preview: Preview = {
   parameters: {
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+      defaultViewport: 'iphone12promax',
+    },
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
